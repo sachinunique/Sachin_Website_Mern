7a240653cb4328d537febaf165e38ffffea056b2
unknown <sachin.uppcl@yahoo.com>
2025-11-09 19:26:47 +0530

    crate pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
+import SignUp from "./Pages/Signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from "./Pages/CreatePost";
+import UpdatePost from "./Pages/UpdatePost";
+import PostPage from "./Pages/PostPage";
+import ScrollToTop from './components/ScrollToTop';
+import Search from "./Pages/Search";
 
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+    <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+        </Route>
+        
+        <Route path="/projects" element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
-
-export default App
